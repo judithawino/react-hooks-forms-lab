a71edbea5180384f9e39a91f316e12670d7be748
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-17 20:37:03 +0300

    react hooks forms lab

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange,search }) {
+  function handleSearchChange(event){
+    onSearchChange(event.target.value);
+  }
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input 
+      type="text" 
+      name={search} 
+      onChange={handleSearchChange}
+      placeholder="Search..."
+      value={search} />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
